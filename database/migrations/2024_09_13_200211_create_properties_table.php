7ea47e03ef125fe00d77cc6a383f63fdedc6a29a
PriyaTechmantra <priya.m@techmantra.co>
2024-09-13 20:05:34 +0530

    admin

---
+++ database/migrations/2024_09_13_200211_create_properties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+           $table->id();
+            $table->string('name');  
+            $table->string('type');  
+            $table->string('address');  
+            $table->decimal('rent', 10, 2);  
+            $table->integer('bedrooms')->nullable();  
+            $table->integer('bathrooms')->nullable();  
+            $table->integer('floor_area')->nullable();  
+            $table->boolean('is_available')->default(true);  
+            $table->text('description')->nullable(); 
+            $table->string('image')->nullable();  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
